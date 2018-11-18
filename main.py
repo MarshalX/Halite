cd02614d0783c87ec48f7c6e312dd163d3000015
Marshal <marshal@xaker.ru>
2018-11-18 05:17:32 +0300

    Добавлено создание графа по матрице инцидентности

--- main.py
+++ main.py
@@ -1,64 +1,73 @@
-#!/usr/bin/env python3
-# Python 3.6
-
-# Import the Halite SDK, which will let you interact with the game.
 import hlt
 
-# This library contains constant values.
 from hlt import constants
-
-# This library contains direction metadata to better interface with the game.
 from hlt.positionals import Direction
 
-# This library allows you to generate random numbers.
 import random
-
-# Logging allows you to save messages for yourself. This is required because the regular STDOUT
-#   (print statements) are reserved for the engine-bot communication.
 import logging
 
-""" <<<Game Begin>>> """
-
-# This game object contains the initial game state.
 game = hlt.Game()
-# At this point "game" variable is populated with initial map data.
-# This is a good place to do computationally expensive start-up pre-processing.
-# As soon as you call "ready" function below, the 2 second per turn timer will start.
+
+game_map = game.game_map
+
+links_count = (game_map.width * game_map.height) * 2
+node_count = game_map.width * game_map.height
+
+graph = [[0 for _ in range(links_count)] for _ in range(node_count)]
+
+current_link = 0
+for node_number in range(node_count):
+    if (node_number + 1) % game_map.height != 0:
+        graph[node_number][current_link] = 1
+        graph[node_number + 1][current_link] = 1
+
+        current_link += 1
+
+    if node_number % game_map.width == 0:
+        graph[node_number][current_link] = 1
+        graph[node_number + game_map.width - 1][current_link] = 1
+
+        current_link += 1
+
+    if node_number < game_map.height:
+        graph[node_number][current_link] = 1
+        graph[game_map.width * game_map.height - game_map.width + node_number][current_link] = 1
+
+        current_link += 1
+
+    if node_number + game_map.width < node_count:
+        graph[node_number][current_link] = 1
+        graph[node_number + game_map.width][current_link] = 1
+
+        current_link += 1
+
+# with open('graph.txt', 'w') as f:
+#     for i in range(node_count):
+#         for j in range(links_count):
+#             symbol = '\n' if j == links_count - 1 else ', '
+#             f.write('{}{}'.format(graph[i][j], symbol))
+
 game.ready("MyPythonBot")
 
-# Now that your bot is initialized, save a message to yourself in the log file with some important information.
-#   Here, you log here your id, which you can always fetch from the game object by using my_id.
 logging.info("Successfully created bot! My Player ID is {}.".format(game.my_id))
 
-""" <<<Game Loop>>> """
-
 while True:
-    # This loop handles each turn of the game. The game object changes every turn, and you refresh that state by
-    #   running update_frame().
     game.update_frame()
-    # You extract player metadata and the updated map metadata here for convenience.
+
     me = game.me
     game_map = game.game_map
 
-    # A command queue holds all the commands you will run this turn. You build this list up and submit it at the
-    #   end of the turn.
     command_queue = []
 
     for ship in me.get_ships():
-        # For each of your ships, move randomly if the ship is on a low halite location or the ship is full.
-        #   Else, collect halite.
         if game_map[ship.position].halite_amount < constants.MAX_HALITE / 10 or ship.is_full:
             command_queue.append(
                 ship.move(
-                    random.choice([ Direction.North, Direction.South, Direction.East, Direction.West ])))
+                    random.choice([Direction.North, Direction.South, Direction.East, Direction.West])))
         else:
             command_queue.append(ship.stay_still())
 
-    # If the game is in the first 200 turns and you have enough halite, spawn a ship.
-    # Don't spawn a ship if you currently have a ship at port, though - the ships will collide.
     if game.turn_number <= 200 and me.halite_amount >= constants.SHIP_COST and not game_map[me.shipyard].is_occupied:
         command_queue.append(me.shipyard.spawn())
 
-    # Send your moves back to the game environment, ending this turn.
     game.end_turn(command_queue)
-
